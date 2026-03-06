2deaad8cfdbf9a0ffdc3101a2d8fe1201a4520ba
Francois <francois.gourbal@gmail.com>
2026-03-06 11:53:58 +0100

    fix: ajouter variable API relative + activer CORS

--- api-events/app.js
+++ api-events/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const db = require('./database');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public')); // Sert le front depuis /public
 
